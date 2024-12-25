f1d45f9f5180c80b7ce7098e5ce88497014286c6
Stefan Schuh <stefan.schuh@obvsg.at>
2024-12-25 21:22:17 +0100

    feat: add reading and conversion functions

--- mrc.el
+++ mrc.el
@@ -1,4 +1,4 @@
-;;; mrc.el --- Description -*- lexical-binding: t; -*-
+;;; mrc.el --- edit MARC 21 in several serializations -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2024 Stefan Schuh
 ;;
@@ -30,6 +30,60 @@ If ARG, only print ARG character positions (or all if ARG is greater than 39)."
         (insert (substring ruler 0 (1+ arg)))
       (insert ruler))))
 
+(defun mrc-read-datafield (mrk)
+  "Create JSON object for a MARC datafield."
+  (let* ((parts (split-string mrk "\\$\\$" t " *"))
+         (tag-ind (replace-regexp-in-string "^=" "" (string-replace " " "" (car parts))))
+         (tag (substring tag-ind 0 3))
+         (ind1 (replace-regexp-in-string "[#\\]" " " (substring tag-ind 3 4)))
+         (ind2 (replace-regexp-in-string "[#\\]" " " (substring tag-ind 4 5)))
+         (subfields (cdr parts)))
+    `((tag . ,tag)
+       (ind1 . ,ind1)
+       (ind2 . ,ind2)
+       (subfields . ,(mrc-handle-subfields subfields)))))
+
+(defun mrc-handle-subfields (subfield-strings)
+  "Create marc:subfields given a marc-breaker string without tag and
+indicators."
+  (vconcat (mapcar #'mrc-read-subfield subfield-strings)))
+
+(defun mrc-read-subfield (sf)
+  "Create a JSON object for a MARC subfield."
+  `((code . ,(substring sf 0 1))
+    (value . ,(string-trim-left (substring sf 1)))))
+;; (cons `((code . ,(substring sf 0 1)) (value . ,(string-trim-left (substring sf 1)))))
+
+(defun mrc-read-controlfield (mrk)
+  "Create JSON object for a MARC controlfield."
+  `((tag . ,(substring mrk 0 3))
+    (value . ,(string-trim-left (substring mrk 3)))))
+
+(defun mrc-pretty-print-json (field)
+  "Serialize and pretty print a FIELD."
+  (with-temp-buffer
+           (insert (json-serialize field))
+           (json-pretty-print (point-min) (point-max))
+           (buffer-string)))
+
+(defun mrc-datafield->dom (datafield)
+  "Convert DATAFIELD to dom object."
+  `(datafield ((tag . ,(alist-get 'tag datafield))
+               (ind1 . ,(alist-get 'ind1 datafield))
+               (ind2 . ,(alist-get 'ind2 datafield)))
+    ,@(mapcar #'mrc-subfield->dom (alist-get 'subfields datafield))))
+
+(defun mrc-subfield->dom (subfield)
+  "Convert SUBFIELD to dom object."
+  `(subfield
+    ((code . , (alist-get 'code subfield)))
+    ,(alist-get 'value subfield)))
+
+(defun mrc-controlfield->dom (controlfield)
+  "Convert CONTROLFIELD to dom object."
+  `(controlfield
+    ((tag . ,(alist-get 'tag controlfield)))
+    ,(alist-get 'value controlfield)))
 
 (defun mrc-line (arg)
   "Convert ARG lines of mark breaker into MARCXML."
